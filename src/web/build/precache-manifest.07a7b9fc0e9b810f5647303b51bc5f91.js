08c448f6b623bf698079840739ee312e1cf94c41
An Le <lehoangthienan@gmail.com>
2019-03-31 16:13:16 +0700

    Add file build

---
+++ src/web/build/precache-manifest.07a7b9fc0e9b810f5647303b51bc5f91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "286d8bb17905ec1a307d",
+    "url": "/static/css/main.6c9c6a2f.chunk.css"
+  },
+  {
+    "revision": "286d8bb17905ec1a307d",
+    "url": "/static/js/main.286d8bb1.chunk.js"
+  },
+  {
+    "revision": "b7f55a57e48fbb9e5c12",
+    "url": "/static/js/1.b7f55a57.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dea0e362a84121a68616d76f1af8a602",
+    "url": "/index.html"
+  }
+];
